aeec43c2cd24eb56a34fb2afa45a5feee674f296
jscheo <981625@naver.com>
2023-10-04 10:20:45 +0900

    2. 글목록 출력

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -4,10 +4,13 @@ import com.example.board.dto.BoardDTO;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +26,13 @@ public class BoardController {
     @PostMapping("/save")
     public String save(@ModelAttribute BoardDTO boardDTO){
         boardService.save(boardDTO);
+        return "redirect:/board";
+    }
+
+    @GetMapping
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
         return "boardPages/boardList";
     }
 }
--- src/main/java/com/example/board/dto/BoardDTO.java
+++ src/main/java/com/example/board/dto/BoardDTO.java
@@ -1,5 +1,6 @@
 package com.example.board.dto;
 
+import com.example.board.entity.BoardEntity;
 import lombok.Data;
 
 @Data
@@ -10,4 +11,15 @@ public class BoardDTO {
     private String boardPass;
     private String boardContents;
     private int boardHits;
+
+    public static BoardDTO toSaveDTO(BoardEntity boardEntity){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        return boardDTO;
+    }
 }
